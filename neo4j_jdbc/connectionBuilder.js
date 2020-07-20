f7a0ba03edc17addb71dc06982f6732d7326d18e
Moxious <moxious@oldhat.org>
2020-07-20 12:33:12 -0400

    umar changes

--- neo4j_jdbc/connectionBuilder.js
+++ neo4j_jdbc/connectionBuilder.js
@@ -1,30 +1,40 @@
 (function dsbuilder(attr) {
-  var urlBuilder =
-    "jdbc:neo4j://" +
-    attr["server"] +
-    ":" +
-    attr["port"] +
-    "?" +
-    "Auth_Type=Basic";
+  var server = attr["server"];
+  var port = attr["port"];
 
-  if (attr["username"] && attr["password"]) {
-    urlBuilder += ";";
-    var params = [];
+  var urlBuilder = (
+    "jdbc:neo4j://" + server + ":" + port + "?"
+  );
+
+  var params = [];
+
+  params["LogLevel"] = "6";
+  params["LogPath"] = "C:\\\\Softwares\\bi_conn";
+  params["Auth_Type"] = "Basic";
+  params["DefaultStringColumnLength"] = "500";
+  params["StrictlyUseBoltScheme"] = "true";
+
+  if (attr["username"] && attr["password"]) { 
     params["PWD"] = attr["password"];
     params["UID"] = attr["username"];
-
-    var formattedParams = [];
-
-    for (var key in params) {
-      formattedParams.push(
-        connectionHelper.formatKeyValuePair(key, params[key])
-      );
-    }
-
-    urlBuilder += formattedParams.join(";");
   }
 
-  //logging.log("combinedurl: " + urlBuilder);
+  var otherOptions = '';
+  var formattedParams = [];
+
+  for (var key in params) {
+    formattedParams.push(
+      connectionHelper.formatKeyValuePair(key, params[key])
+    );
+  }
+
+  urlBuilder += formattedParams.join(";");
+
+  if (otherOptions) {
+    urlBuilder += ";" + otherOptions;
+  }
+
+  logging.Log("combinedurl: " + urlBuilder);
 
   return [urlBuilder];
 });
